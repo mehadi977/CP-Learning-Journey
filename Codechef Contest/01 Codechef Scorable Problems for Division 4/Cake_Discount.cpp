243c8fd514b766f72588152ae35801bbd80191c4
Mehadi Hassan <128689640+mehadi977@users.noreply.github.com>
2026-05-11 15:22:41 +0600

    Add files via upload

---
+++ Codechef Contest/01 Codechef Scorable Problems for Division 4/Cake_Discount.cpp
@@ -0,0 +1,16 @@
+// Problem Link:
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 5)
+        cout << n * 100;
+    else
+        cout << n * 85;
+
+    return 0;
+}

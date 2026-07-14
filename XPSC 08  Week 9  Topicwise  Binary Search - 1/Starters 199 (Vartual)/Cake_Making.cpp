c898785a8904310a1b4372c6da92180370355476
Mehadi Hassan <128689640+mehadi977@users.noreply.github.com>
2026-07-14 12:22:02 +0600

    Add files via upload

---
+++ XPSC 08  Week 9  Topicwise  Binary Search - 1/Starters 199 (Vartual)/Cake_Making.cpp
@@ -0,0 +1,30 @@
+// Problem Link:
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve__()
+{
+    int a, b;
+    cin >> a >> b;
+    int mn = min(a, b);
+    cout << (a * b) - mn << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve__();
+
+    return 0;
+}
---
+++ XPSC 08  Week 9  Topicwise  Binary Search - 1/Starters 199 (Vartual)/Fence_Colouring.cpp
@@ -0,0 +1,49 @@
+// Problem Link:
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve__()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        mp[v[i]]++;
+    }
+    int mx = 1;
+    for (auto [a, b] : mp)
+    {
+        mx = max(mx, b);
+    }
+    // if (mp.size() == 1)
+    //     cout << 1 << endl;
+    // else
+    cout << min(n - mx + 1, n - mp[1]) << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve__();
+    }
+
+    return 0;
+}
